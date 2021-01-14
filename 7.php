cab19fcdf77191a3e4b86d5ee10e23ff497bec53
harry2405 <77462332+harry2405@users.noreply.github.com>
2021-01-15 01:29:42 +0530

    Add files via upload

---
+++ 7.php
@@ -0,0 +1,20 @@
+<DOCTYPE HTML>
+<html>
+<head>
+<meta http-equiv="refresh" content="1"/>
+<style>
+p{
+color:black;
+font-size:70px;
+position:absolute;
+transform:translate(-50%,-50%);
+top:50%;
+left:50%;
+}
+body{
+background-color:white;
+}
+</style>
+<p>
+<?php echo date("h:i:sA");?></p>
+</head>
